b114cb60d28479d5e20feaea375c71344e5ef18c
asmraihan <asmraihanctg14@gmail.com>
2023-05-09 13:52:23 +0600

    done

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,26 @@
 
+import { useLoaderData } from 'react-router-dom'
 import './App.css'
+import CoffeeCard from './components/CoffeeCard'
+import { useState } from 'react'
 
 function App() {
-
+  const loadedCoffees = useLoaderData()
+  const [coffees, setCoffees] = useState(loadedCoffees)
   return (
-    <>
-     <h1 className='text-6xl text-purple-600'>coffee</h1>
-    </>
+    <div className='m-20'>
+     <h1 className='text-6xl text-purple-600 text-center my-20'>HOT COLD COFFEE : {coffees.length}</h1>
+   <div className='grid md:grid-cols-2 gap-4'>
+   {
+      coffees.map(coffee=><CoffeeCard
+      key={coffee._id}
+      coffee={coffee}
+      coffees={coffees}
+      setCoffees={setCoffees}
+      ></CoffeeCard>)
+     }
+   </div>
+    </div>
   )
 }
 
